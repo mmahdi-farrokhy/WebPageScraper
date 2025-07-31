785c760c1d16edb147463c1bea1dfa70f600fecf
mmahdi-farrokhy-BPJ <m.farokhi@behpardaz.net>
2025-07-31 12:29:34 +0330

    created spring boot api

---
+++ WebPageScraperAPI/src/main/java/org/mmf/webpagescraperapi/WebPageScraperApiApplication.java
@@ -0,0 +1,13 @@
+package org.mmf.webpagescraperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageScraperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPageScraperApiApplication.class, args);
+    }
+
+}
---
+++ WebPageScraperAPI/src/test/java/org/mmf/webpagescraperapi/WebPageScraperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mmf.webpagescraperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPageScraperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
